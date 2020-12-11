fd06b6f060597b95c180d3702ab70c73e04ffdae
subin <subini1707@khu.ac.kr>
2020-12-11 12:34:54 +0900

    snow delete

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,5 @@
 import React from 'react';
 import './Home.css?v=1.0';
-import './snow.css';
-
-
-const Snowflake = ({ style }) => {
-    return (
-      <p className="snow-flake" style={style}>
-        {"\u2745"}
-      </p>
-    );
-  };
-  
-  const makeSnowFlakes = () => {
-    let animationDelay = "0.1s"; 
-    let fontSize = "14px"; 
-    const arr = Array.from("Merry Christmas"); 
-  
-   
-    return arr.map((el, i) => {
-      animationDelay = `${(Math.random() * 80).toFixed(100)}s`; 
-      fontSize = `${Math.floor(Math.random() * 10) + 25}px`; 
-      const style = {
-        animationDelay,
-        fontSize,
-      };
-    return <Snowflake key={i} style={style} />;  });
-  };
-  
-
-
 
 const Home = ( { history } ) => 
 {
@@ -36,9 +7,6 @@ const Home = ( { history } ) =>
     return (
         <>
             <div>
-                <div className="snow-container">
-                {makeSnowFlakes()}
-                </div>
                 <head>
                     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css"></link>
                 </head>
